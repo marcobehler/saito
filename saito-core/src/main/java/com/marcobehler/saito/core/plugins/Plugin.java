f5edb4aa871f60063d386547b3ef75f95021c71c
Marco Behler <marco@marcobehler.com>
2016-04-06 18:32:39 +0200

    [saito-cli] plugin cleanup

--- saito-core/src/main/java/com/marcobehler/saito/core/plugins/Plugin.java
+++ saito-core/src/main/java/com/marcobehler/saito/core/plugins/Plugin.java
@@ -2,20 +2,18 @@ package com.marcobehler.saito.core.plugins;
 
 import com.marcobehler.saito.core.Saito;
 
-import java.util.Comparator;
-
 /**
  * @author Marco Behler <marco@marcobehler.com>
  */
-public interface Plugin extends Comparator<Plugin> {
+public interface Plugin extends Comparable<Plugin> {
 
     void start(Saito saito);
 
     Integer getOrder();
 
     @Override
-    default int compare(Plugin o1, Plugin o2) {
-        return o1.getOrder().compareTo(o2.getOrder());
+    default int compareTo(Plugin o) {
+        return getOrder().compareTo(o.getOrder());
     }
 
 }
---
+++ saito-core/src/test/java/com/marcobehler/saito/core/plugins/PluginTest.java
@@ -0,0 +1,33 @@
+package com.marcobehler.saito.core.plugins;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author Marco Behler <marco@marcobehler.com>
+ */
+public class PluginTest {
+
+    @Test
+    public void test_ordering() {
+        Set<Plugin> plugins = new HashSet<>();
+
+        FileWatcherPlugin fileWatcherPlugin = new FileWatcherPlugin();
+        plugins.add(fileWatcherPlugin);
+
+        JettyPlugin jettyPlugin = new JettyPlugin();
+        plugins.add(jettyPlugin);
+
+        LiveReloadPlugin liveReloadPlugin = new LiveReloadPlugin();
+        plugins.add(liveReloadPlugin);
+
+        TreeSet<Plugin> sortedPlugins = new TreeSet<>(plugins);
+        assertThat(sortedPlugins.first()).isEqualTo(jettyPlugin);
+        assertThat(sortedPlugins.last()).isEqualTo(liveReloadPlugin);
+    }
+}
