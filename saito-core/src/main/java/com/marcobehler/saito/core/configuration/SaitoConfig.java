07654d25d17ef83dbdf96d2ee290fb7cee9bff8a
Marco Behler <marco@marcobehler.com>
2016-04-09 23:33:17 +0200

    [saito-core]  config tests and refactoring

--- saito-core/src/main/java/com/marcobehler/saito/core/configuration/SaitoConfig.java
+++ saito-core/src/main/java/com/marcobehler/saito/core/configuration/SaitoConfig.java
@@ -1,5 +1,6 @@
 package com.marcobehler.saito.core.configuration;
 
+import com.marcobehler.saito.core.Saito;
 import com.marcobehler.saito.core.dagger.PathsModule;
 import com.marcobehler.saito.core.freemarker.FreemarkerConfig;
 import dagger.Lazy;
@@ -13,8 +14,10 @@ import org.yaml.snakeyaml.representer.Representer;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Map;
 
 /**
  * @author Marco Behler <marco@marcobehler.com>
@@ -42,15 +45,19 @@ public class SaitoConfig {
         initializeFromYaml(configFile);
     }
 
+
     @SneakyThrows
     private void initializeFromYaml(@Named(PathsModule.CONFIG_FILE) Path configFile) {
-        if (!Files.exists(configFile)) {
+        if (configFile == null || !Files.exists(configFile)) {
             log.info("No config file {} found - using defaults", configFile);
             return;
         }
-        Representer r = new Representer();
-        r.represent(this);
-        Yaml yaml = new Yaml(r);
-        yaml.load(new String(Files.readAllBytes(configFile), "UTF-8"));
+        Yaml yaml = new Yaml();
+
+        Map<String, Object> map = (Map<String, Object>) yaml.load(new String(Files.readAllBytes(configFile), "UTF-8"));
+        directoryIndexes = (boolean) map.getOrDefault("directoryIndexes", directoryIndexes);
+        relativeLinks = (boolean) map.getOrDefault("relativeLinks", relativeLinks);
+        liveReloadEnabled = (boolean) map.getOrDefault("liveReloadEnabled", liveReloadEnabled);
+        port = (Integer) map.getOrDefault("port", port);
     }
 }
--- saito-core/src/test/java/com/marcobehler/saito/core/configuration/SaitoConfigTest.java
+++ saito-core/src/test/java/com/marcobehler/saito/core/configuration/SaitoConfigTest.java
@@ -5,41 +5,65 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 /**
  * @author Marco Behler <marco@marcobehler.com>
  */
 public class SaitoConfigTest extends AbstractInMemoryFileSystemTest {
 
-    @Before
-    public void setup() {
+    @Test
+    public void get_default_config_with_null_path_returns_default() {
+        SaitoConfig config = new SaitoConfig(null, null);
 
+        assertThat(config.isDirectoryIndexes()).isEqualTo(false);
+        assertThat(config.isRelativeLinks()).isEqualTo(false);
     }
 
     @Test
-    public void get_default_config_with_null_path() {
-       /* SaitoConfig defaultConfig = SaitoConfig.getOrDefault(null);
+    public void get_default_config_with_non_existing_path_returns_default() {
+        SaitoConfig config = new SaitoConfig(Paths.get("/bla.yaml"), null);
 
-        assertThat(defaultConfig.isDirectoryIndexes()).isEqualTo(false);
-        assertThat(defaultConfig.isRelativeLinks()).isEqualTo(false);*/
-    }
-
-    @Test
-    public void get_default_config_with_non_existing_path() {
-      /*  SaitoConfig defaultConfig = SaitoConfig.getOrDefault(Paths.get("/bla.yaml"));
-
-        assertThat(defaultConfig.isDirectoryIndexes()).isEqualTo(false);
-        assertThat(defaultConfig.isRelativeLinks()).isEqualTo(false);*/
+        assertThat(config.isDirectoryIndexes()).isEqualTo(false);
+        assertThat(config.isRelativeLinks()).isEqualTo(false);
     }
 
 
     @Test
-    public void get_config_from_path() throws IOException {
-      /*  Path configFile = Files.write(fs.getPath("/test.yaml"), "directoryIndexes : true\nrelativeLinks: true".getBytes("UTF-8"));
+    public void get_config_from_path_directory_indexes() throws IOException {
+        Path configFile = Files.write(fs.getPath("/test.yaml"), "directoryIndexes : true".getBytes("UTF-8"));
 
-        SaitoConfig defaultConfig = SaitoConfig.getOrDefault(configFile);
+        SaitoConfig config = new SaitoConfig(configFile, null);
+        assertThat(config.isDirectoryIndexes()).isEqualTo(true);
+    }
 
-        assertThat(defaultConfig.isDirectoryIndexes()).isEqualTo(true);
-        assertThat(defaultConfig.isRelativeLinks()).isEqualTo(true);*/
+
+    @Test
+    public void get_config_from_path_relative_links() throws IOException {
+        Path configFile = Files.write(fs.getPath("/test.yaml"), "relativeLinks: true".getBytes("UTF-8"));
+
+        SaitoConfig config = new SaitoConfig(configFile, null);
+        assertThat(config.isRelativeLinks()).isEqualTo(true);
+    }
+
+
+    @Test
+    public void get_config_from_path_port() throws IOException {
+        Path configFile = Files.write(fs.getPath("/test.yaml"), "port : 9999".getBytes("UTF-8"));
+
+        SaitoConfig config = new SaitoConfig(configFile, null);
+        assertThat(config.getPort()).isEqualTo(9999);
+    }
+
+    @Test
+    public void get_config_from_path_livereload() throws IOException {
+        Path configFile = Files.write(fs.getPath("/test.yaml"), "liveReloadEnabled : true".getBytes("UTF-8"));
+
+        SaitoConfig config = new SaitoConfig(configFile, null);
+        assertThat(config.isLiveReloadEnabled()).isEqualTo(true);
     }
 }
