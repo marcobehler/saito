428c4d0dd6f7b102a6574ffb177daaf4366e20ff
BEHLEMA <marco.behler@kvb.de>
2016-05-06 14:52:11 +0200

    [saito-core] work on freemarker

--- saito-core/src/test/java/com/marcobehler/saito/core/rendering/FreemarkerRendererTest.java
+++ saito-core/src/test/java/com/marcobehler/saito/core/rendering/FreemarkerRendererTest.java
@@ -7,12 +7,22 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 
 import com.marcobehler.saito.core.BaseInMemoryFSTest;
+import com.marcobehler.saito.core.Saito;
+import com.marcobehler.saito.core.configuration.SaitoConfig;
 import com.marcobehler.saito.core.files.Layout;
 import com.marcobehler.saito.core.files.Template;
+import com.marcobehler.saito.core.freemarker.FreemarkerModule;
 import com.marcobehler.saito.core.freemarker.FreemarkerRenderer;
 import com.marcobehler.saito.core.freemarker.FreemarkerTemplateLoader;
+import com.marcobehler.saito.core.util.LinkHelper;
 
+import dagger.Lazy;
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.cache.MultiTemplateLoader;
+import freemarker.cache.TemplateLoader;
+import freemarker.template.Configuration;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  *
@@ -43,7 +53,7 @@ public class FreemarkerRendererTest extends BaseInMemoryFSTest {
 
 
     @Test
-    public void render_works() throws IOException {
+    public void simple_render_works() throws IOException {
         String templateFileName = "index.ftl";
         Files.write(workingDirectory.resolve(templateFileName), ("---\n" + "layout: layout\n" + "---This is not a test").getBytes());
 
@@ -53,7 +63,43 @@ public class FreemarkerRendererTest extends BaseInMemoryFSTest {
         Template saitoTemplate = new Template(workingDirectory, fs.getPath("index.ftl"));
         saitoTemplate.setLayout(new Layout(workingDirectory, fs.getPath("layout.ftl")));
 
-        String rendered = new FreemarkerRenderer(new FreemarkerTemplateLoader(null)).render(saitoTemplate, null);
+        String rendered = new FreemarkerRenderer(new FreemarkerTemplateLoader(freemarkerConfig())).render(saitoTemplate, new RenderingModel(mock(SaitoConfig.class)));
         assertThat(rendered).isEqualTo("<p>This is not a test</p>");
+    }
+
+
+    @Test
+    public void frontMatter_variable_works() throws IOException {
+        String templateFileName = "index.ftl";
+        Files.write(workingDirectory.resolve(templateFileName), ("---\n" + "layout: layout\ntitle: Das ist mein title" + "---This is not a test, ${title}").getBytes());
+
+        String layoutFileName = "layout.ftl";
+        Files.write(workingDirectory.resolve(layoutFileName), ("<p>[@saito.yield/]</p>").getBytes());
+
+        Template saitoTemplate = new Template(workingDirectory, fs.getPath("index.ftl"));
+        saitoTemplate.setLayout(new Layout(workingDirectory, fs.getPath("layout.ftl")));
+
+        String rendered = new FreemarkerRenderer(new FreemarkerTemplateLoader(freemarkerConfig())).render(saitoTemplate, new RenderingModel(mock(SaitoConfig.class)));
+        assertThat(rendered).isEqualTo("<p>This is not a test, Das ist mein title</p>");
+    }
+
+
+    @Test
+    public void nested_frontMatter_variable_works() throws IOException {
+        String templateFileName = "index.ftl";
+        Files.write(workingDirectory.resolve(templateFileName), ("---\n" + "layout: layout\ntitle:\n  new: neuer title\n  old: alter title" + "---This is not a test, ${title.old}").getBytes());
+
+        String layoutFileName = "layout.ftl";
+        Files.write(workingDirectory.resolve(layoutFileName), ("<p>[@saito.yield/]</p>").getBytes());
+
+        Template saitoTemplate = new Template(workingDirectory, fs.getPath("index.ftl"));
+        saitoTemplate.setLayout(new Layout(workingDirectory, fs.getPath("layout.ftl")));
+
+        String rendered = new FreemarkerRenderer(new FreemarkerTemplateLoader(freemarkerConfig())).render(saitoTemplate, new RenderingModel(mock(SaitoConfig.class)));
+        assertThat(rendered).isEqualTo("<p>This is not a test, alter title</p>");
+    }
+
+    private Lazy<Configuration> freemarkerConfig() {
+        return () -> FreemarkerModule.configuration(mock(LinkHelper.class), new MultiTemplateLoader(new TemplateLoader[]{new ClassTemplateLoader(Saito.class.getClassLoader(), "/")}));
     }
 }
