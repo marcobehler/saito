fd6556a082cdc38616b1e68086abfaecf2d79e12
BEHLEMA <marco.behler@kvb.de>
2016-07-26 15:57:28 +0200

    [saito-core] tests for blogpost

--- saito-core/src/main/java/com/marcobehler/saito/core/rendering/RenderingModule.java
+++ saito-core/src/main/java/com/marcobehler/saito/core/rendering/RenderingModule.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.marcobehler.saito.core.asciidoc.AsciidocRenderer;
 import com.marcobehler.saito.core.freemarker.FreemarkerRenderer;
 import com.marcobehler.saito.core.markdown.MarkdownRenderer;
 
@@ -21,7 +22,7 @@ public class RenderingModule {
 
     @Provides(type = SET_VALUES)
     @Singleton
-    static Set<Renderer> renderers(FreemarkerRenderer freemarkerRenderer, MarkdownRenderer markdownRenderer) {
-        return new HashSet<>(Arrays.asList(freemarkerRenderer, markdownRenderer));
+    static Set<Renderer> renderers(FreemarkerRenderer freemarkerRenderer, MarkdownRenderer markdownRenderer, AsciidocRenderer asciidocRenderer) {
+        return new HashSet<>(Arrays.asList(freemarkerRenderer, markdownRenderer, asciidocRenderer));
     }
 }
