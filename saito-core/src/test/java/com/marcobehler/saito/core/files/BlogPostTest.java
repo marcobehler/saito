b06441918e6e3ae5bfa41ff7f059f3539f41a5f3
BEHLEMA <marco.behler@kvb.de>
2016-07-26 16:05:35 +0200

    [saito-core] tests for blogpost

--- saito-core/src/test/java/com/marcobehler/saito/core/files/BlogPostTest.java
+++ saito-core/src/test/java/com/marcobehler/saito/core/files/BlogPostTest.java
@@ -3,12 +3,23 @@ package com.marcobehler.saito.core.files;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
 import com.marcobehler.saito.core.BaseInMemoryFSTest;
+import com.marcobehler.saito.core.configuration.SaitoConfig;
+import com.marcobehler.saito.core.freemarker.FreemarkerRenderer;
+import com.marcobehler.saito.core.freemarker.FreemarkerTemplateLoader;
+import com.marcobehler.saito.core.rendering.Renderer;
+import com.marcobehler.saito.core.rendering.RenderingEngine;
+import com.marcobehler.saito.core.rendering.RenderingModel;
 
+import static com.marcobehler.saito.core.rendering.FreemarkerRendererTest.freemarkerConfig;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 /**
  *
@@ -45,6 +56,29 @@ public class BlogPostTest extends BaseInMemoryFSTest  {
 
         assertThat(post.getTitle()).isEqualTo("this-is-it");
     }
+
+
+    @Test
+    public void blog_post_should_be_processed_into_correct_directory() throws IOException, BlogPost.BlogPostFormattingException {
+        String templateFileName = "2015-03-05-this-is-it.html.ftl";
+        Files.write(workingDirectory.resolve(templateFileName), ("---\n" + "layout: layout\n" + "---This is not a test").getBytes());
+
+        String layoutFileName = "layout.ftl";
+        Files.write(workingDirectory.resolve(layoutFileName), ("<p>[@saito.yield/]</p>").getBytes());
+
+        Template saitoTemplate = new Template(workingDirectory, fs.getPath(templateFileName));
+        saitoTemplate.setLayout(new Layout(workingDirectory, fs.getPath(layoutFileName)));
+
+        final BlogPost bp = new BlogPost(workingDirectory, fs.getPath(templateFileName));
+
+        // TODO
+        /*final RenderingModel renderingModel = new RenderingModel(mock(SaitoConfig.class));
+        bp.process(renderingModel, workingDirectory.resolve("output"), new RenderingEngine(mock(SaitoConfig.class), new HashSet<Renderer>(FreemarkerRenderer)));
+
+        String rendered = new FreemarkerRenderer(new FreemarkerTemplateLoader(freemarkerConfig())).render(saitoTemplate, );
+        assertThat(rendered).isEqualTo("<p>This is not a test</p>");*/
+    }
+
 
     @Test
     public void blog_post_should_parse_filename_correctly_independent_from_extension()
